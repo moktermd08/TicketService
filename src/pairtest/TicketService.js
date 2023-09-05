1d35665669b33e96a74dff9faade7e13bcc10821
Mo <55099505+moktermd08@users.noreply.github.com>
2023-09-05 16:17:17 +0200

    Update TicketService.js
    added error handling an exceptions to TicketService.js

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -15,14 +15,23 @@ class TicketService {
    * @param {Array} ticketTypeRequests - List of TicketTypeRequest objects describing the tickets to be purchased.
    */
   purchaseTickets(accountId, ...ticketTypeRequests) {
-    // Validation and Calculation Logic
-    const { totalCost, totalSeats } = this.#validateAndCalculate(ticketTypeRequests);
+    try {
+      // Validation and Calculation Logic
+      const { totalCost, totalSeats } = this.#validateAndCalculate(ticketTypeRequests);
 
-    // Make a payment request via TicketPaymentService
-    TicketPaymentService.processPayment(accountId, totalCost);
+      // Try making a payment request via TicketPaymentService
+      if (!TicketPaymentService.processPayment(accountId, totalCost)) {
+        throw new Error('Payment processing failed.');
+      }
 
-    // Reserve seats via SeatReservationService
-    SeatReservationService.reserveSeats(accountId, totalSeats);
+      // Try reserving seats via SeatReservationService
+      if (!SeatReservationService.reserveSeats(accountId, totalSeats)) {
+        throw new Error('Seat reservation failed.');
+      }
+    } catch (e) {
+      // Re-throw as a custom exception for unified handling
+      throw new InvalidPurchaseException(e.message);
+    }
   }
 
   /**
@@ -38,33 +47,36 @@ class TicketService {
     const ticketPricing = { 'ADULT': 20, 'CHILD': 10, 'INFANT': 0 };
 
     // Loop through each ticket request to validate and calculate costs
-    ticketTypeRequests.forEach(request => {
+    for (const request of ticketTypeRequests) {
       // Validate ticket type
       if (!ticketPricing.hasOwnProperty(request.type)) {
         throw new InvalidPurchaseException('Invalid ticket type.');
       }
 
-      // Update adult ticket count for later validation
-      if (request.type === 'ADULT') adultCount += request.count;
-
-      // Update child and infant ticket count for later validation
-      if (['CHILD', 'INFANT'].includes(request.type)) childAndInfantCount += request.count;
-
       // Validate ticket count
-      if (request.count <= 0) throw new InvalidPurchaseException('Ticket count should be greater than zero.');
+      if (request.count <= 0) {
+        throw new InvalidPurchaseException('Ticket count should be greater than zero.');
+      }
 
-      // Calculate total cost and seats
+      // Update counts for adult, child, and infant tickets
+      adultCount += (request.type === 'ADULT') ? request.count : 0;
+      childAndInfantCount += (['CHILD', 'INFANT'].includes(request.type)) ? request.count : 0;
+
+      // Calculate cost and seats
       totalCost += ticketPricing[request.type] * request.count;
       if (request.type !== 'INFANT') totalSeats += request.count;
-    });
+    }
 
     // Validate total ticket count against the business rule
-    if (totalSeats > 20) throw new InvalidPurchaseException('Cannot purchase more than 20 tickets.');
+    if (totalSeats > 20) {
+      throw new InvalidPurchaseException('Cannot purchase more than 20 tickets.');
+    }
 
     // Validate the requirement of at least one adult ticket if any child or infant tickets are purchased
-    if (childAndInfantCount > 0 && adultCount === 0) throw new InvalidPurchaseException('Child and Infant tickets require at least one Adult ticket.');
+    if (childAndInfantCount > 0 && adultCount === 0) {
+      throw new InvalidPurchaseException('Child and Infant tickets require at least one Adult ticket.');
+    }
 
-    // Return the calculated totals for further action
     return { totalCost, totalSeats };
   };
 }
